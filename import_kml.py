e2a70611fd68b35dafc1b38df24a8be60ffd8597
P4radise <emel.9616@gmail.com>
2021-08-04 16:30:33 +0700

    IHub-179434 Initial implementation

--- import_kml.py
+++ import_kml.py
@@ -29,6 +29,9 @@ class Integration:
             parse_list = self.csv.parse(Integration.KML_FILENAME)
             self.csv.create(parse_list, Integration.CSV_FILENAME)
             self.integration_import.import_process(Integration.CSV_FILENAME)
+        except Exception as e:
+            self.integration_log.add(LogLevel.ERROR, str(e))
+            raise e
         finally:
             self.delete_files()
 
