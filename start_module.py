8bcf98daec247cfd1b76c473838ad3ee02a03df3
P4radise <emel.9616@gmail.com>
2022-04-18 21:44:49 +0700

    Add automatic installation of dependencies

--- start_module.py
+++ start_module.py
@@ -4,4 +4,4 @@ import subprocess
 
 
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'python_dependencies.txt'])
-os.system(f'python3 module_settings.py')
+os.system('python3 module_settings.py')
