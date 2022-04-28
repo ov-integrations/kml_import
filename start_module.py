fc565bfd2d39cb324dfa7d890d371ff9931a6551
P4radise <emel.9616@gmail.com>
2022-04-28 11:01:58 +0700

    IHub-214263 Add automatic installation of dependencies

--- start_module.py
+++ start_module.py
@@ -1,7 +1,11 @@
 import os
 import sys
+import json
 import subprocess
 
 
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'python_dependencies.txt'])
-os.system('python3 module_settings.py')
+with open('settings.json', 'rb') as PFile:
+    settings_data = json.loads(PFile.read().decode('utf-8'))
+
+os.system(settings_data['commandStart'])
